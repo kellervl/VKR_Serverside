d30fd4e9d7eda629df37342c3ea0d4268b4141c7
kellervl <44319134+kellervl@users.noreply.github.com>
2019-11-17 22:13:34 +0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -4,11 +4,10 @@ var mysql      = require('mysql');
 //connection
 var connection = mysql.createConnection({
   database : 'Project',
-  host     : 'localhost',
+  host     : '46.29.209.239',
   user     : 'root',
   password : 'mysqlpassword',
-  port : '3306',
-  host : 'localhost'
+  port : '3306'
 });
 connection.connect(
     function(err) {
