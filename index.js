9f13d7a62a04e91cc54222709f41b51440311da4
kellervl <44319134+kellervl@users.noreply.github.com>
2019-11-20 10:19:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.listen(8000, function () {
 
 //CORS middleware
 app.use(function(req, res, next) {
-  var allowedOrigins = ['http://localhost:3000']
+  var allowedOrigins = ['https://project-control.herokuapp.com/']
   res.header('Access-Control-Allow-Origin', allowedOrigins);
   res.header( 'Access-Control-Allow-Headers', 'withCredentials, Access-Control-Allow-Headers, Origin, X-Requested-With, X-AUTHENTICATION, X-IP, Content-Type, Accept, Access-Control-Request-Method, Access-Control-Request-Headers');
   res.header( 'Access-Control-Allow-Methods', 'GET, OPTIONS, HEAD, POST, PUT, DELETE');
